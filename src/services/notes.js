f16da6315085623a1d6fd717fb61846bdab944c2
Leandro <lsmarques@gmail.com>
2020-08-17 15:28:04 -0300

    Vai

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = REACT_APP_API_URL + '/api/notes'
+const baseUrl = process.env.REACT_APP_API_URL + '/api/notes'
 
 // const getAll = () => {
 //   const request = axios.get(baseUrl)
